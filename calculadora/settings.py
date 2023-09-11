6db41e7cf443863ec428fc195c0ce901899d30e2
Excel-ente <turkienich@gmail.com>
2023-09-11 00:38:28 -0300

    cambios

--- calculadora/settings.py
+++ calculadora/settings.py
@@ -297,7 +297,7 @@ JAZZMIN_UI_TWEAKS = {
     "brand_small_text": False,
     "brand_colour": False,
     "accent": "accent-success",
-    "navbar": "navbar-white navbar-light",
+    "navbar": "navbar-dark",
     "no_navbar_border": False,
     "navbar_fixed": False,
     "layout_boxed": False,
@@ -310,11 +310,11 @@ JAZZMIN_UI_TWEAKS = {
     "sidebar_nav_compact_style": False,
     "sidebar_nav_legacy_style": True,
     "sidebar_nav_flat_style": False,
-    "theme": "sketchy",
-    "dark_mode_theme": "cyborg",
+    "theme": "solar",
+    "dark_mode_theme": None,
     "button_classes": {
-        "primary": "btn-outline-primary",
-        "secondary": "btn-outline-secondary",
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
         "info": "btn-info",
         "warning": "btn-warning",
         "danger": "btn-danger",
@@ -322,7 +322,6 @@ JAZZMIN_UI_TWEAKS = {
     },
     "actions_sticky_top": True
 }
-
 '''
 JAZZMIN_UI_TWEAKS = {
     "navbar_small_text": False,
